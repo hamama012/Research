08bcea91816666f3f5fb5d180917865863c2b510
Haruna Nakajima <inuwanwan2@gmail.com>
2017-11-28 14:55:17 +0900

    01:ちょっと見やすくしただけ

--- Research_Project/Assets/MovePet.cs
+++ Research_Project/Assets/MovePet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MovePet : MonoBehaviour {
 	public float speed = 3.0f; //速度
-	private float rotationSmooth = 1.0f;
+    private float rotationSmooth = 0.1f;
 	private float changeTargetSqrDistance = 10.0f; //目標位置を切り替える距離
 
 	public bool  wander = true; //徘徊行動判定
@@ -20,19 +20,26 @@ public class MovePet : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
+        //目標位置
 		float sqrDistanceToTarget = Vector3.SqrMagnitude (transform.position - targetPosition);
 		if (sqrDistanceToTarget < changeTargetSqrDistance) {
 			targetPosition = GetRandomPositionOnLevel ();
 		}
 
+        //回転実行
 		Quaternion targetRotation = Quaternion.LookRotation (targetPosition - transform.position);
 		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, Time.deltaTime * rotationSmooth);
 
+        //アニメーション
 		animator.SetBool ("walk", true);
+
+        //移動実行
 		transform.Translate (Vector3.forward * speed * Time.deltaTime);
 	
 	}
 
+    //目標位置設定
 	public Vector3 GetRandomPositionOnLevel()
 	{
 		float levelSize = 55f;
